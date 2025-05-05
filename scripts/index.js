0bc24e70c63b8a56b51fba92269eadebbfa7eafa
clane83 <clane83@yahoo.com>
2025-05-05 16:46:41 -0700

    updated menu to show hambuger and disappear on large css

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,14 @@
-
-document.getElementById("currentYear").innerHTML = new Date().getFullYear();
-
-document.getElementById("lastModified").textContent = "Last Modification: " + new Date(document.lastModified).toLocaleString();
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("currentYear").innerHTML = new Date().getFullYear();
+    
+    document.getElementById("lastModified").textContent = "Last Modification: " + new Date(document.lastModified).toLocaleString();
+    
+    // Hamburger menu
+    const hamButton = document.querySelector("#menu");
+        const navigation = document.querySelector(".navigation");
+    
+        hamButton.addEventListener("click", () => {
+            navigation.classList.toggle("open");
+            hamButton.classList.toggle("open");
+        });
+    });
